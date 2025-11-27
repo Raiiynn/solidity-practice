d2cca27c4717a8a3366860ed0d1436bb941e8177
Raiiynn <githubdaffa@gmail.com>
2025-11-28 00:50:05 +0700

    solidity: sending and receiving eth

---
+++ simplewallet.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.30;
+
+contract SimpleWallet {
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function deposit() public payable{}
+
+    function getBalance() public view returns (uint) {
+        return address(this).balance;
+    }
+
+    function withdraw(uint amount) public {
+        require(msg.sender == owner, "Only owner can call this");
+        payable(msg.sender).transfer(amount);
+    }
+}
